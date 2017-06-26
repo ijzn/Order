741d0daf52c3758e944a4809cf181e87ccdca412
jzn <592193834@qq.com>
2017-06-26 16:24:52 +0800

    vue 练手项目，更加熟悉vue   感谢胖哥，

---
+++ src/components/common/leftNav.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="leftNav">
+        <ul id="leftNav">
+            <li>
+                <i class="icon iconfont icon-dache"></i>
+                <div>打车</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-dache"></i>
+                <div>外卖</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-dache"></i>
+                <div>快餐</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-dache"></i>
+                <div>SPA</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-dache"></i>
+                <div>快递</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-dache"></i>
+                <div>足疗</div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default{
+    name:'leftNav',
+    data () {
+        return {
+            
+        }
+    }
+};
+</script>
+
+<style scoped>
+.leftNav{
+    color:#fff;
+    font-size:20px;
+    background-color: #1D8ce0;
+    float:left;
+    width:5%;
+    height:100%;
+}
+.iconfont{
+    font-size:50px;
+}
+.leftNav ul{
+    padding:0px;
+    margin: 0px;
+    height:100%
+}
+.leftNav li{
+    list-style: none;
+    text-align: center;
+    border-bottom:1px solid #20a0ff;
+    height:100px;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction:column;
+}
+</style>
